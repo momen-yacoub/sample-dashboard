3f31e76e64a95c5744d3f5659a860e39dd628f19
moamen saeed <momenfci@gmail.com>
2020-05-30 10:08:52 +0200

    add vendor name to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :vendor_id, :price, :quantity
+  attributes :id, :name, :vendor_id, :price, :quantity, :vendor_name
+
+  def vendor_name
+    object.vendor&.name
+  end
 end
